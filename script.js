a361e623e66b78afbbe9606dbf07fb7598dc3f2c
LAGstudi0 <paolo.milutinovic@gmail.com>
2023-09-25 11:02:06 +0100

    Unbeatable AI done

--- script.js
+++ script.js
@@ -36,10 +36,23 @@ function startGame() {
 
 }
 
+function getRandomEmptyCellIndex() {
+    const emptyCells = [...cellElements].filter(cell => !cell.classList.contains(X_CLASS) && !cell.classList.contains(CIRCLE_CLASS));
+    if (emptyCells.length === 0) return null; // No empty cells left
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    return Array.from(cellElements).indexOf(emptyCells[randomIndex]);
+}
+
 function handleClick(e) {
     const cell = e.target;
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
+
+    // Log the index of the cell pressed.
+    const cellIndex = Array.from(cell.parentElement.children).indexOf(cell);
+    console.log(`Clicked cell index: ${cellIndex}`);
+
+
     if (checkWin(currentClass)) {
         endGame(false);
     } else if (isDraw()) {
@@ -47,6 +60,17 @@ function handleClick(e) {
     } else {
         swapTurns()
         setBoardHoverClass()
+
+        // Add AI's turn here
+        if (circleTurn) { 
+            setTimeout(() => {
+                const emptyCellIndex = bestMove();
+                if (emptyCellIndex !== null) {
+                    const emptyCell = cellElements[emptyCellIndex];
+                    emptyCell.click(); // Simulate a click on the empty cell
+                }
+            }, 100); // Delay the AI's move for 1 second (adjust as needed)
+        }
     }
 }
 
@@ -89,4 +113,73 @@ function checkWin(currentClass) {
             return cellElements[index].classList.contains(currentClass)
         })
     })
+}
+
+
+function evaluate(board) {
+    if (checkWin(CIRCLE_CLASS)) {
+        return 10;
+    } else if (checkWin(X_CLASS)) {
+        return -10;
+    } 
+    return 0;
+}
+
+function minimax(board, depth, isMaximizing) {
+    const scores = {
+        X: -10,
+        O: 10,
+        draw: 0,
+    };
+
+    if (checkWin(CIRCLE_CLASS)) {
+        return scores.O;
+    }
+    if (checkWin(X_CLASS)) {
+        return scores.X;
+    }
+    if (isDraw()) {
+        return scores.draw;
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 9; i++) {
+            if (!cellElements[i].classList.contains(X_CLASS) && !cellElements[i].classList.contains(CIRCLE_CLASS)) {
+                cellElements[i].classList.add(CIRCLE_CLASS);
+                const score = minimax(board, depth + 1, false);
+                cellElements[i].classList.remove(CIRCLE_CLASS);
+                bestScore = Math.max(score, bestScore);
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < 9; i++) {
+            if (!cellElements[i].classList.contains(X_CLASS) && !cellElements[i].classList.contains(CIRCLE_CLASS)) {
+                cellElements[i].classList.add(X_CLASS);
+                const score = minimax(board, depth + 1, true);
+                cellElements[i].classList.remove(X_CLASS);
+                bestScore = Math.min(score, bestScore);
+            }
+        }
+        return bestScore;
+    }
+}
+
+function bestMove() {
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 9; i++) {
+        if (!cellElements[i].classList.contains(X_CLASS) && !cellElements[i].classList.contains(CIRCLE_CLASS)) {
+            cellElements[i].classList.add(CIRCLE_CLASS);
+            const score = minimax(board, 0, false);
+            cellElements[i].classList.remove(CIRCLE_CLASS);
+            if (score > bestScore) {
+                bestScore = score;
+                move = i;
+            }
+        }
+    }
+    return move;
 }
